028c9d9fe4d1cd6ad95ebc9e138bc94fd5643499
Rishabh Sharma <mrvishumahant@gmail.com>
2026-02-27 20:48:15 +0530

    Day 1 - Introduction to JavaScript

---
+++ 01-introduction/practice.js
@@ -0,0 +1,11 @@
+// Day 1 - Introduction to JavaScript
+
+// Printing output
+console.log("Hello World");
+
+// Variables using let
+let name = "Rishabh";
+let year = 3;
+
+console.log("I am " + name);
+console.log("I am in year " + year);
