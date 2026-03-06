f626d6809039c957070965d37aa07c3d7c07e5b4
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-06 23:03:59 +0530

    Day 2 - JavaScript Data Types Practice

---
+++ 02-data-types/practice.js
@@ -0,0 +1,22 @@
+// Day 2 - JavaScript Data Types
+
+// String
+let name = "Rishabh";
+
+// Number
+let age = 21;
+
+// Boolean
+let isStudent = true;
+
+// Undefined
+let city;
+
+// Null
+let car = null;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(city);
+console.log(car);
