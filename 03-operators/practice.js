d7972fc0edfa8ac92366191e0049e3d85544ee18
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-07 20:02:41 +0530

    Day 3 - JavaScript Operators Practice

---
+++ 03-operators/practice.js
@@ -0,0 +1,23 @@
+// Day 3 - JavaScript Operators
+
+let a = 10;
+let b = 5;
+
+// Arithmetic Operators
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+// Comparison Operators
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a == b:", a == b);
+
+// Logical Operators
+let x = true;
+let y = false;
+
+console.log("x && y:", x && y);
+console.log("x || y:", x || y);
+console.log("!x:", !x);
