0d1dc2a6f02a0e4b18cefbbba7ee90c1b6b2943a
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-09 23:23:37 +0530

    Day 4 - JavaScript Conditionals Practice

---
+++ 04-conditionals/practice.js
@@ -0,0 +1,28 @@
+// Day 4 - JavaScript Conditionals
+
+let age = 20;
+
+// if statement
+if (age >= 18) {
+  console.log("You are eligible to vote");
+}
+
+// if-else statement
+let number = 5;
+
+if (number % 2 === 0) {
+  console.log("Even number");
+} else {
+  console.log("Odd number");
+}
+
+// else-if example
+let marks = 75;
+
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 70) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
