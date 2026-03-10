6f43cde9f83e42fa6d85f99e090ac63366a9b655
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-10 21:44:47 +0530

    Day 5 - JavaScript Loops Practice

--- 05-loops/practice.js
+++ 05-loops/practice.js
@@ -1,1 +1,20 @@
+// Day 5 - JavaScript Loops
 
+// for loop
+for (let i = 1; i <= 5; i++) {
+  console.log("For Loop:", i);
+}
+
+// while loop
+let j = 1;
+while (j <= 5) {
+  console.log("While Loop:", j);
+  j++;
+}
+
+// do...while loop
+let k = 1;
+do {
+  console.log("Do While Loop:", k);
+  k++;
+} while (k <= 5);
