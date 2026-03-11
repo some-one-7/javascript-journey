0d06df48055ba1103885220888c0f8465b29237e
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-11 23:15:58 +0530

    Day 6 - JavaScript Functions Practice

--- 06-functions/practice.js
+++ 06-functions/practice.js
@@ -1,1 +1,23 @@
+// Day 6 - JavaScript Functions
 
+// Simple function
+function greet() {
+  console.log("Hello, welcome to JavaScript!");
+}
+
+greet();
+
+// Function with parameters
+function add(a, b) {
+  console.log("Sum:", a + b);
+}
+
+add(5, 3);
+
+// Function with return value
+function multiply(x, y) {
+  return x * y;
+}
+
+let result = multiply(4, 6);
+console.log("Multiplication:", result);
