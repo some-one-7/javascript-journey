b0b2d76617926d0f37d89b32097c09d66813d45d
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-12 19:38:27 +0530

    Day 7 - JavaScript Arrays Practice

---
+++ 07-arrays/practice.js
@@ -0,0 +1,19 @@
+// Day 7 - JavaScript Arrays
+
+// Creating an array
+let fruits = ["Apple", "Banana", "Mango"];
+
+// Accessing elements
+console.log(fruits[0]);
+console.log(fruits[1]);
+
+// Adding an element
+fruits.push("Orange");
+console.log(fruits);
+
+// Removing last element
+fruits.pop();
+console.log(fruits);
+
+// Array length
+console.log("Array length:", fruits.length);
