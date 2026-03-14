16b7425aa946ae05e8ede2241a2874713b4c0a22
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-14 17:31:30 +0530

    Practicinng through objects
    Added examples of looping through an object and counting its keys.

--- 09-Objects/problems.js
+++ 09-Objects/problems.js
@@ -1,1 +1,30 @@
 
+// looping through object
+const phone = {
+    brand: "Apple",
+    model: "iPhone 14",
+    price: 80000
+};
+
+for(let key in phone){
+    console.log(key + ":", phone[key]);
+}
+
+// Counting number of keys
+
+const user = {
+    name: "Rishi",
+    age: 20,
+    city: "Delhi",
+    profession: "Student"
+};
+
+let count = 0;
+
+for(let key in user){
+    count++;
+}
+
+console.log("Total properties:", count);
+
+//
