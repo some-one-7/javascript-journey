78cdfe4653993c62554a621ca880a4dc5b8064a7
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-22 08:08:55 +0530

    Update script.js

--- 14-async-javascript-basics/script.js
+++ 14-async-javascript-basics/script.js
@@ -1,1 +1,43 @@
+// Timer (1 to 10 using setInterval)
+let timerBtn = document.getElementById("startTimer");
+let timerDisplay = document.getElementById("timerDisplay");
 
+timerBtn.addEventListener("click", () => {
+  let count = 1;
+
+  let timer = setInterval(() => {
+    timerDisplay.innerText = count;
+    count++;
+
+    if (count > 10) {
+      clearInterval(timer);
+    }
+  }, 1000);
+});
+
+// Delayed Alert using setTimeout
+let delayBtn = document.getElementById("delayBtn");
+
+delayBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    alert("This alert appeared after 3 seconds!");
+  }, 3000);
+});
+
+// Interval Start/Stop
+let startInterval = document.getElementById("startInterval");
+let stopInterval = document.getElementById("stopInterval");
+let intervalDisplay = document.getElementById("intervalDisplay");
+
+let interval;
+let num = 0;
+
+startInterval.addEventListener("click", () => {
+  interval = setInterval(() => {
+    intervalDisplay.innerText = num++;
+  }, 1000);
+});
+
+stopInterval.addEventListener("click", () => {
+  clearInterval(interval);
+});
