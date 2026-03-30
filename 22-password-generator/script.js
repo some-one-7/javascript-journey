1c056971c52bf76d87cd2f4de2b16dc0ba165fc6
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-30 19:40:28 +0530

    Update script.js

--- 22-password-generator/script.js
+++ 22-password-generator/script.js
@@ -1,1 +1,23 @@
+const generateBtn = document.getElementById("generate");
+const result = document.getElementById("result");
 
+const chars =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+
+generateBtn.addEventListener("click", () => {
+  const length = document.getElementById("length").value;
+
+  if (!length || length <= 0) {
+    result.innerText = "Enter valid length!";
+    return;
+  }
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    password += chars[randomIndex];
+  }
+
+  result.innerText = password;
+});
