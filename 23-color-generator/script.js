65d60655f77309194e009ed1bbe7f3b14cd7acf6
Rishabh Sharma <mrvishumahant@gmail.com>
2026-03-31 07:41:01 +0530

    Implement random color generator on button click
    Add functionality to generate and display random colors.

--- 23-color-generator/script.js
+++ 23-color-generator/script.js
@@ -1,1 +1,22 @@
+const btn = document.getElementById("btn");
+const colorCode = document.getElementById("colorCode");
 
+btn.addEventListener("click", () => {
+  const randomColor = generateColor();
+
+  document.body.style.backgroundColor = randomColor;
+  colorCode.innerText = randomColor;
+});
+
+// Function to generate random hex color
+function generateColor() {
+  const chars = "0123456789abcdef";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    let randomIndex = Math.floor(Math.random() * chars.length);
+    color += chars[randomIndex];
+  }
+
+  return color;
+}
