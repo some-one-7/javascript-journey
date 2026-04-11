ef3207d39f8ef43bcf496e284f283228ed0c8c33
Rishabh Sharma <mrvishumahant@gmail.com>
2026-04-11 23:36:38 +0530

    Update main.js

--- Day 33-Modular-JS/main.js
+++ Day 33-Modular-JS/main.js
@@ -1,1 +1,28 @@
+import { getTransactions, saveTransactions } from "./storage.js";
+import { renderUI } from "./ui.js";
+import { addTransaction } from "./transactions.js";
 
+const form = document.getElementById("form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+
+let transactions = getTransactions();
+
+// Initial render
+renderUI(transactions);
+
+// Add transaction
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  transactions = addTransaction(transactions, {
+    id: Date.now(),
+    text: text.value,
+    amount: +amount.value
+  });
+
+  saveTransactions(transactions);
+  renderUI(transactions);
+
+  form.reset();
+});
