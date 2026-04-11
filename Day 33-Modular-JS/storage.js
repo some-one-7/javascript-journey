61d72a3fb8325dd81f0c6c73338b1e59952896b3
Rishabh Sharma <mrvishumahant@gmail.com>
2026-04-11 23:43:13 +0530

    Update storage.js

--- Day 33-Modular-JS/storage.js
+++ Day 33-Modular-JS/storage.js
@@ -1,1 +1,7 @@
+export function getTransactions() {
+  return JSON.parse(localStorage.getItem("transactions")) || [];
+}
 
+export function saveTransactions(transactions) {
+  localStorage.setItem("transactions", JSON.stringify(transactions));
+}
