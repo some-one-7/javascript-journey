b4c8064bbaa75b83f60e423a783119be62d540a1
Rishabh Sharma <mrvishumahant@gmail.com>
2026-04-11 23:40:54 +0530

    Update ui.js

--- Day 33-Modular-JS/ui.js
+++ Day 33-Modular-JS/ui.js
@@ -1,1 +1,15 @@
+export function renderUI(transactions) {
+  const list = document.getElementById("list");
+  list.innerHTML = "";
 
+  transactions.forEach(t => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+      ${t.text}
+      <span>₹${t.amount}</span>
+    `;
+
+    list.appendChild(li);
+  });
+}
