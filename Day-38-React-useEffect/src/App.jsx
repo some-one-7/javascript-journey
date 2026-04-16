fc9f12a3e2d72315715d88f0bcf671a2ea899d2d
Rishabh Sharma <mrvishumahant@gmail.com>
2026-04-16 22:43:06 +0530

    Day 38: Add useEffect auto fetch

--- Day-38-React-useEffect/src/App.jsx
+++ Day-38-React-useEffect/src/App.jsx
@@ -1,17 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function App() {
-  const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("octocat");
 
-  const fetchUser = async () => { const res = await fetch("https://api.github.com/users/${username}"); const data = await res.json(); console.log(data); };
+  const fetchUser = async () => {
+    const res = await fetch(`https://api.github.com/users/${username}`);
+    const data = await res.json();
+    console.log(data);
+  };
+
+  // ✅ Auto fetch on page load
+  useEffect(() => {
+    fetchUser();
+  }, []);
 
   return (
-    <div className="container">
-      <h1>GitHub Auto Fetch</h1>
+    <div>
+      <h1>GitHub Finder</h1>
 
       <input
         type="text"
-        placeholder="Enter username"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
