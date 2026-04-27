ec68290bdfa3d35efe1229cb6607a8b94fee8fc6
Rishabh Sharma <mrvishumahant@gmail.com>
2026-04-27 21:36:05 +0530

    Update todoReducer.js

--- Day-44-useReducer/src/reducer/todoReducer.js
+++ Day-44-useReducer/src/reducer/todoReducer.js
@@ -1,1 +1,27 @@
+export const initialState = {
+  todos: []
+};
 
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      return {
+        todos: [...state.todos, { id: Date.now(), text: action.payload, done: false }]
+      };
+
+    case "DELETE":
+      return {
+        todos: state.todos.filter((todo) => todo.id !== action.payload)
+      };
+
+    case "TOGGLE":
+      return {
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+        )
+      };
+
+    default:
+      return state;
+  }
+};
