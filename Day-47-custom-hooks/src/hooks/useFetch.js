74f8a5500ddf142b21e1140242f617b4a167f030
Vishu <mrvishumahant@gmail.com>
2026-05-01 07:54:57 +0530

    Created useFetch.js

---
+++ Day-47-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(url);
+
+        if (!res.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        const result = await res.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useFetch;
