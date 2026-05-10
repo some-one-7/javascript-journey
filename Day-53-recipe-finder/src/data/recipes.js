4bd6c8147ec51c1b0d837f9824930550751e5a8f
Vishu <mrvishumahant@gmail.com>
2026-05-10 09:05:55 +0530

    updated recipe.js

---
+++ Day-53-recipe-finder/src/data/recipes.js
@@ -0,0 +1,19 @@
+const recipes = [
+  {
+    id: 1,
+    title: "Paneer Curry",
+    description: "Delicious spicy paneer curry",
+  },
+  {
+    id: 2,
+    title: "Veg Biryani",
+    description: "Loaded vegetable biryani",
+  },
+  {
+    id: 3,
+    title: "Pasta",
+    description: "Creamy white sauce pasta",
+  },
+];
+
+export default recipes;
