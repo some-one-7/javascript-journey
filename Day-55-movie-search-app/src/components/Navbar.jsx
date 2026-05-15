20bf89bf220fffc8ce84e62b9cb7ed45bd955068
Vishu <mrvishumahant@gmail.com>
2026-05-15 20:36:10 +0530

    updated Navbar.jsx

---
+++ Day-55-movie-search-app/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Link to="/">Movie Search App</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
